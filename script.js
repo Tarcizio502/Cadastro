4e9075fddd462ebd8f5062cf8ea7cd946cd3f548
tarcizio502 <tarciziogoncalves502@gmail.com>
2025-04-12 12:23:41 -0300

    Foi adicionado cpf, email, data de nascimento .....

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-document.addEventListener('DOMContentLoaded',()=>{
-let nome = document.querySelector('.nome') 
+document.addEventListener('DOMContentLoaded',()=>{  
+   
+let nome = document.querySelector('.nome')  
+let cpf = document.querySelector('.cpf') 
+let data = document.querySelector('.data') 
 let idade =document.querySelector('.idade') 
+let cidade = document.querySelector('.cidade')  
+let email = document.querySelector('.email')  
 let genero = document.querySelector('.genero')
 let btn = document.querySelector('.btn') 
 let infos = document.querySelector('.infos') 
 let arr = []
 
 btn.addEventListener('click',()=>{  
-  const pessoa = { 
-    nome:nome.value,
-    idade:idade.value, 
+  const pessoa = {  
+    nome:nome.value,  
+    cpf:cpf.value,  
+    data:data.value,
+    idade:idade.value,   
+    cidade:cidade.value, 
+    email:email.value,
     genero:genero.value, 
   } 
   arr.push(pessoa)
   infos.innerHTML = ``
   arr.forEach((elemento,index)=>{
     infos.innerHTML += `
-    <p>chegada: ${index+1}º</p>
-    <p>nome: ${elemento.nome}</p>
-    <p>idade: ${elemento.idade}</p> 
-    <p>genero: ${elemento.genero}</p> 
+    <p>Chegada: ${index+1}º</p>  
+    <p>Nome: ${elemento.nome}</p>  
+    <p>CPF: ${elemento.cpf}</p>  
+    <p>Data: ${elemento.data}</p>
+    <p>Idade: ${elemento.idade}</p>  
+    <p>Cidade: ${elemento.cidade}<p>
+    <p>Email: ${elemento.email}<p/>
+    <p>Gênero: ${elemento.genero}</p> 
     <hr>
     `
 
